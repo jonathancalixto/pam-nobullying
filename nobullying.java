39d1f972470a33bcb32313f135397053766e677b
JONATHAN ROBERTO CALIXTO PESSOA <550773095@alunos.etec.itaqua>
2019-03-14 20:17:14 -0300

    Escrevi o 5 passo

--- nobullying.java
+++ nobullying.java
@@ -5,6 +5,7 @@ public class NoBullying {
 		System.out.println("passo 2 : observar o comportamendo das pessoa ao seu redor ");
 		System.out.println("passo 3 : Sempre  conversa com pessoas que aparenta ta triste ");
 		System.out.println("passo 4 : ajudar o proximo ");
+		System.out.println("passo 5 : saber o que esta acontecendo ");
 		
 		
 	}
