53a27f707000116a7893ba3a944a3e25bf9d2d08
JONATHAN ROBERTO CALIXTO PESSOA <550773095@alunos.etec.itaqua>
2019-03-14 20:14:13 -0300

    Escrevi o 3 passo

--- nobullying.java
+++ nobullying.java
@@ -1,8 +1,9 @@
 public class NoBullying {  
 	public static void main(String[] args){   
-		System.out.println('Programa NoBullying'); 
+		System.out.println("Programa NoBullying"); 
 		System.out.println("passo 1 : respeitar as Opiniões do individuo ");
 		System.out.println("passo 2 : observar o comportamendo das pessoa ao seu redor ");
+		System.out.println("passo 3 : Sempre  conversa com pessoas que aparenta ta triste ");
 		
 		
 	}
