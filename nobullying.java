2241a1fd04eebb341cff34cee9d80b0c16f537a1
JONATHAN ROBERTO CALIXTO PESSOA <550773095@alunos.etec.itaqua>
2019-03-14 20:05:29 -0300

    Escrevi o passo 1

--- nobullying.java
+++ nobullying.java
@@ -1,5 +1,9 @@
 public class NoBullying {  
 	public static void main(String[] args){   
-		System.out.println('Programa NoBullying');  
+		System.out.println('Programa NoBullying'); 
+		System.out.println("passo 1 : respeitar as Opiniões do individuo ");
+		
+		
+		
 	}
  } 
